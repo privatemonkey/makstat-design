323fc95e3f5afd90fc9d0e359f98bb9c09ca2ea7
Anders Olsen <andersnolsen@gmail.com>
2022-11-10 10:23:17 +0100

    Initial commit
    Basic project setup

--- README.md
+++ README.md
@@ -1,2 +1,14 @@
-# makstat-design
-Design for the MakStat website
+# Readme
+
+## Files
+
+### Assets
+
+Files that should be used for production
+
+
+
+
+## Templating
+
+To include a component we use a simple ´´´data-include="path/filename.html"´´´attribute. A script runs on load and adds components to the page.
---
+++ templating.js
@@ -0,0 +1,65 @@
+async function getFile(el, file) {
+  await fetch(file, {
+    cache: "no-store"
+  })
+  .then((response) => {
+    if (response.status == 200) {
+      return response.text()
+    } else {
+      throw new Error(response.status);
+    }
+  }
+  )
+  .then((data) => {
+    if (data) {
+      el.insertAdjacentHTML('beforebegin', data);
+      el.textContent = data.split('>').join('>\n')
+      el.removeAttribute('data-include')
+      el.className = 'code is-hidden'
+    } else {
+      if (data==='') {
+        el.innerHTML = "Is empty '" + file + "'";
+        el.removeAttribute('data-include')
+        el.className = "template-is-empty"
+      } else {
+        el.innerHTML = "Could not find '" + file + "'";
+        el.removeAttribute('data-include')
+        el.className = "template-load-error"
+      }
+    }
+  }).catch((err) => {
+    console.log('Error')
+    console.log(err)
+  })
+}
+
+
+async function includeHTML() {
+    var includes, i, el, file;
+    
+    includes = document.querySelectorAll('[data-include]');
+    
+    for (i = 0; i < includes.length; i++) {
+      el = includes[i];
+      
+      file = el.getAttribute("data-include");
+      
+      try {
+        await getFile(el, file)
+      } catch(err) {
+        console.log(err)
+      }
+    }
+}
+
+function toggleCode() {
+  let code = document.querySelectorAll('.code')
+  
+  code.forEach(el => {
+      if (el.className.indexOf('is-hidden')>-1) {
+        el.className = 'code'
+      } else {
+      el.className = 'code is-hidden'
+      } 
+  })
+}
